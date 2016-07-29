507c4f537f869ec68b2ab8c6b0b88964cc992226
eunseokchoi <cesboy@naver.com>
2016-07-29 09:46:21 +0900

    Update first.java
    추가했습니다 확인하세요~

--- first.java
+++ first.java
@@ -2,6 +2,12 @@ class  first
 {
 	public static void main(String[] args) 
 	{
+	
 		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+		
 	}
 }
